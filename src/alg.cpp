bc0fdeb0b0a84db9cbf3cbb6ad3ef0da7d6a5f21
Dolman1 <78943568+Dolman1@users.noreply.github.com>
2021-03-29 22:20:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,25 +1,6 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int i = 0; 
-  int j = size - 1;
-  int c = 0; //counter
-  while (i < j) {
-    int middle = (i + j) / 2;
-    if (arr[middle] < value)
-      i = middle + 1;
-    else
-      j = middle;
-    if (arr[i] == value) {
-      while (arr[i] == value) {
-        c++;
-        i++;
-      }
-    }
-  }
-  if (c > 0)
-    return c;
-  else
+  //  поместить сюда реализацию алгоритма
   return 0; // если ничего не найдено
-  
 }
